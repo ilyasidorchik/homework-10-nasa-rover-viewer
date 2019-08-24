73c262b5301d6facd469b26c8f2a85d453ba60ba
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-24 14:29:26 +0300

    Adds auth reducer

--- src/modules/Auth/auth.js
+++ src/modules/Auth/auth.js
@@ -1,1 +1,12 @@
-// Реализуйте редьюсер
+import { handleActions } from 'redux-actions';
+
+import { addKey } from './actions';
+
+const apiKey = handleActions(
+    {
+        [addKey]: (_state, action) => action.payload
+    },
+    null
+);
+
+export default apiKey;
